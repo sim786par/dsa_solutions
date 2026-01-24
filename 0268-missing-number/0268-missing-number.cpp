bb86ea3fadd3d422e926deffbb9fe118cbb9cc99
Simran Parween <simranparween2754@gmail.com>
2026-01-24 08:27:42 +0530

    Update 0268-missing-number.cpp
    all approaches added

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,3 +1,40 @@
+// this is brute force approach n^2 
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n= nums.size();
+//         for(int i=0;i<n;i++){
+//             int found = false;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j] == i){
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if(!found){
+//                 return i;
+//             }
+
+//         } 
+//         return nums.size();
+//     }
+// };
+
+// using sort function nlogn
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i < nums.size();i++){
+//             if(i != nums[i]){
+//                 return i;
+//             }
+//         }
+//         return nums.size();
+//     }
+// };
+
+// optimized solution using x-or operation O(n)
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {

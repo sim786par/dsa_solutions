56f6501daf6362b1c00dce2711617a2ef99c9871
Simran Parween <simranparween2754@gmail.com>
2026-07-11 13:44:42 +0530

    Time: 0 ms (100%), Space: 99 MB (87.3%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    // int findCenter(vector<vector<int>>& edges) {
+    //     int n = edges.size()+1;
+    //     vector<vector<int>> adj(n+1);
+    //     for(auto edge : edges){
+    //         int u = edge[0];
+    //         int v = edge[1];
+    //         adj[u].push_back(v);
+    //         adj[v].push_back(u);
+    //     }
+    //     for(int i=1;i<=n;i++){
+    //         if(adj[i].size() == n-1){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size()+1;
-        vector<vector<int>> adj(n+1);
-        for(auto edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+
+        if (a == c || a == d) {
+            return a;
         }
-        for(int i=1;i<=n;i++){
-            if(adj[i].size() == n-1){
-                return i;
-            }
-        }
-        return -1;
+        return b;
     }
 };

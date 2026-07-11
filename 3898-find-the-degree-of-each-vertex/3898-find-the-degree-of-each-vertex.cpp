ff7e4b3bfc269bf10e02cb67fbf622340e6e0bd1
Simran Parween <simranparween2754@gmail.com>
2026-07-11 12:25:31 +0530

    Time: 0 ms (100%), Space: 54.5 MB (44.45%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<int> findDegrees(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<int> ans(n,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j <n;j++){
-                if(matrix[i][j] ==1 && i != j){                   
-                    ans[i]++;
-                }
-            }
-        }
-        return ans;
+        //t.c = o(n^2)
+        // int n = matrix.size();
+        // vector<int> ans(n,0);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j <n;j++){
+        //         if(matrix[i][j] ==1 && i != j){                   
+        //             ans[i]++;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //t.c = o(n) using stl accumulate which return the sum of the values in the list
+        int n=matrix.size();
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++)
+        ans[i]=accumulate(matrix[i].begin(),matrix[i].end(),0);
+         return ans; 
     }
 };

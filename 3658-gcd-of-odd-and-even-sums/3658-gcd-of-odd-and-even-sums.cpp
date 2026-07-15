91eb5d89c7c0799e97d1c1b0594acf517f419a5f
Simran Parween <simranparween2754@gmail.com>
2026-07-15 09:30:02 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.4%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumodd = 0,sumeven =0,i=1;
+    //Approach 1 O(n) first sum of odd and even then find gcd
+        // int sumodd = 0,sumeven =0,i=1;
+        // while(i != 2*n){
+        //     if(i%2 == 0){
+        //         sumeven +=i;
+        //     }
+        //     else {
+        //         sumodd = sumodd + i;
+        //     }
+        //     i++;
+        // }
+        // while(sumodd != 0){
+        //     int temp = sumodd;
+        //     sumodd = sumeven % sumodd;
+        //     sumeven = temp;
+        // }
+        // return sumeven;
 
-        while(i != 2*n){
-            if(i%2 == 0){
-                sumeven +=i;
-            }
-            else {
-                sumodd = sumodd + i;
-            }
-            i++;
-        }
-        while(sumodd != 0){
-            int temp = sumodd;
-            sumodd = sumeven % sumodd;
-            sumeven = temp;
-        }
-        return sumeven;
+    //Approach 2 mathematical O(1) sum of odd = n^2 sum of even = n(n-1) gcd(n^2,n(n-1)) = gcd(n,n-1) and consecutive no is co prime so result n hi hoga
+        return n;
     }
 };
